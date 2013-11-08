11008d15370d092ea800be06862db3fd88518b87
Justin Reynard <framerate@gmail.com>
2013-11-08 08:50:55 -0800

    Commented and properly indented Gruntfile.js

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -1,70 +1,98 @@
-module.exports = function (grunt) {
-    grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+/*
+SECRET COW LEVEL - HTML5 Gruntfile Configuration
+Author: justin@secretcowlevel.com
+Date: 11.08.2013
+ */
 
-    // define source files and their destinations
-    uglify: 
+module.exports = function (grunt) {
+    grunt.initConfig(
     {
-        options: {
-            mangle: true,
-            compress: true,
-            banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-                    '<%= grunt.template.today("yyyy-mm-dd") %> */'
+        // Give Grunt access to the package.json file info
+        pkg: grunt.file.readJSON('package.json'),
+
+        // this will uglify/minify/concat our files
+        uglify: 
+        {
+            options: 
+            {
+                mangle: true,
+                compress: true,
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                        '<%= grunt.template.today("yyyy-mm-dd") %> */'
+            },
+            js: 
+            {
+                files: 
+                {
+                    'static/js/scl.min.js': ['lib/client/js/*']
+                }
+            }
         },
-        js: {
-            files: {
-                'static/js/scl.min.js': 
-                    [
-                        'lib/client/js/*'
-                    ]
+
+        // We use this to make sure our JS files follow our spec
+        jshint: 
+        {
+            server: 
+            {
+                src: ['lib/server/js/*'],
+                options: 
+                { 
+                    node: true
+                }
+            },
+            client:
+            {
+                src: ['lib/client/js/*'],
+                options: 
+                { 
+                    globalstrict: true 
+                }
+            }
+        },
+
+        // Watch for changes to trigger events
+        watch: 
+        {
+            client:  
+            { 
+                files: ['lib/client/js/*.js', 'lib/server/js/*.js', './*.js' ], 
+                tasks: [ 'jshint', 'uglify'] 
+            },
+        },
+
+        // restart node server, if necessary
+        nodemon: 
+        {
+            dev: 
+            {
+                options: 
+                {
+                    file: 'app.js',
+                }
+            }
+        },
+
+        // This allows us to concurrently watch JS for tasks and nodemon to restart
+        concurrent:
+        {
+            dev: 
+            {
+                tasks: ['nodemon', 'watch'],
+                options: 
+                {
+                    logConcurrentOutput: true
+                }
             }
         }
-        
-    },
+    });
 
-   jshint: {
-       server: {
-         src: ['lib/server/js/*'],
-         options: { 
-            node: true
-         }
-       },
-       client: {
-         src: ['lib/client/js/*'],
-         options: { 
-            globalstrict: true 
-        }
-       }
-     },
-    watch: {
-        client:  { files: ['lib/client/js/*.js', 'lib/server/js/*.js', './*.js' ], tasks: [ 'jshint', 'uglify'] },
-    },
-    nodemon: {
-      dev: {
-        options: {
-          file: 'app.js',
-        }
-      }
-    },
-    concurrent: {
-      dev: {
-        tasks: ['nodemon', 'watch'],
-        options: {
-          logConcurrentOutput: true
-        }
-      }
-    }
-});
+    // Load the plugins we want to use
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-concurrent');
 
-// load plugins
-grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.loadNpmTasks('grunt-contrib-jshint');
-grunt.loadNpmTasks('grunt-nodemon');
-grunt.loadNpmTasks('grunt-concurrent');
-
-// register at least this one task
-grunt.registerTask('default', [ 'jshint', 'uglify', 'concurrent' ]);
-
-
+    // Register the tasks to run
+    grunt.registerTask('default', [ 'jshint', 'uglify', 'concurrent' ]);
 };
