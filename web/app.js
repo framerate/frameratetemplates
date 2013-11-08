0c575edafe0c87b07ce3c86fe0ee68ced65998d6
Justin Reynard <framerate@gmail.com>
2013-11-08 10:29:49 -0800

    Added stock EJS template

--- web/app.js
+++ web/app.js
@@ -4,6 +4,10 @@ var express = require('express');
 
 var app = express();
 
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+app.use(express.static('static'));
+
 require('./lib/routes')(app);
 
 app.listen(3000);
--- web/lib/routes.js
+++ web/lib/routes.js
@@ -4,9 +4,6 @@ module.exports = function(app)
 {
 	app.get('/', function(req, res)
 	{
-	  var body = 'Hello World';
-	  res.setHeader('Content-Type', 'text/plain');
-	  res.setHeader('Content-Length', body.length);
-	  res.end(body);
+		res.render('index.ejs', {text: 'Hello World'});
 	});
 };
