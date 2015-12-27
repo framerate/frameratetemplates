2bb06c3c87203d206e46d4110925a64bc4bdc7a1
Justin Reynard <framerate@gmail.com>
2015-12-27 13:14:40 -0600

    wtf hapi

--- server/app.js
+++ server/app.js
@@ -1,122 +1,63 @@
-'use strict';
-
-var config = global.config = require('../Config.json');
-var express = require('express');
-var session = require('express-session');
-var RedisStore = require('connect-redis')(session);
-var expressHandlebars = require('express-handlebars');
-var path = require('path');
-var serveStatic = require('serve-static');
-
-var mongoose = require('mongoose');
-var uuid = require('node-uuid');
-
-var app = express();
-var http = require('http');
-var isProduction = process.env.NODE_ENV === 'production' || false;
-var port = isProduction ? 8080 : 3000;
-
-var server;
-
-var uri = require('./helpers/mongodb').createURI(config.databases.mongo);
-mongoose.connect(uri, config.databases.mongo.options);
-
-// webpack real time stuff
-if (!isProduction) {
-    var httpProxy = require('http-proxy');
-    var proxy = httpProxy.createProxyServer({
-        changeOrigin: true
-    });
-    // We require the bundler inside the if block because
-    // it is only needed in a development environment. Later
-    // you will see why this is a good idea
-    var bundle = require('./bundle.js');
-    bundle();
-
-    app.all('/socket.io*', function (req, res) {
-        proxy.web(req, res, {
-            target: 'http://127.0.0.1:3001'
-        });
-    });
-    // Any requests to localhost:3000/build is proxied
-    // to webpack-dev-server
-    app.get('/js/*.js', function (req, res) {
-        proxy.web(req, res, {
-            target: 'http://localhost:8080/'
-        });
-    });
-
-
-    proxy.on('error', function () {
-        // Just catch it
-    });
-
-    // We need to use basic HTTP service to proxy
-    // websocket requests from webpack
-    server = http.createServer(app);
-
-    server.on('upgrade', function (req, socket, head) {
-        proxy.ws(req, socket, head);
-    });
-}
-
-var sessionTTLSeconds = 1 * 24 * 60 * 60;
-var storeOptions = {
-    host: config.databases.redis.server,
-    port: config.databases.redis.port,
-    db: config.databases.redis.database || 0,
-    ttl: sessionTTLSeconds
-};
-if (config.databases.redis.password) {
-    storeOptions.pass = config.databases.redis.password;
-}
-app.use(serveStatic(path.join(__dirname, '../../static'), {index: false, redirect: true}));
-app.use(session({
-    store: new RedisStore(storeOptions),
-    genid: function () {
-        return uuid.v4();
-    },
-    secret: 'ff4a588e-0e3b-4698-817c-7191de36862c',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        maxAge: sessionTTLSeconds * 1000
-    }
-}));
-
-
-
-
-var handlebars = expressHandlebars.create({
-    layoutsDir: path.join(__dirname, './views/layout'),
-    partialsDir: path.join(__dirname, './views/partial'),
-    defaultLayout: 'main'
-
-});
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, './views'));
-
-// @TODO - move this
-var React = require('react');
-var ReactDOM = require('react-dom/server');
-require('babel/register');
-var Header = React.createFactory(require('../src/js/components/Header.jsx'));
-var Footer = React.createFactory(require('../src/js/components/Footer.jsx'));
-
-app.get('*', function (request, response) {
-    var headerHTML = ReactDOM.renderToStaticMarkup(Header({}));
-    var footerHTML = ReactDOM.renderToStaticMarkup(Footer({}));
-    return response.render('root', {
-        header: headerHTML,
-        footer: footerHTML
-    });
+require('babel-register')({
+  presets: ['es2015', 'react']
 });
 
-if (!isProduction) {
 
-    server.listen(port, function () {
+var Hapi = require('hapi');
+var dateFormat = require('dateformat');
+var format = 'dd mmm HH:MM:ss';
+
+// Basic Hapi.js connection stuff
+var server = new Hapi.Server();
+server.connection({
+  host: 'localhost',
+  port: 8000
+});
+
+// Register the inert and vision Hapi plugins
+// As of Hapi 9.x, these two plugins are no longer
+// included in Hapi automatically
+// https://github.com/hapijs/hapi/issues/2682
+server.register([{
+  register: require('inert')
+}, {
+  register: require('vision')
+}], function(err) {
+
+  if (err) return console.error(err);
+
+    // Add the React-rendering view engine
+    server.views({
+        engines: {
+            jsx: require('hapi-react-views')
+        },
+        relativeTo: __dirname,
+        path: 'views'
     });
-} else {
-    server = app.listen(port);
-}
+
+    // Add a route to serve static assets (CSS, JS, IMG)
+    server.route({
+      method: 'GET',
+      path: '/{param*}',
+      handler: {
+        directory: {
+          path: 'static',
+          index: ['index.html']
+        }
+      }
+    });
+
+    // Add main app route
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: {
+        view: 'Default'
+      }
+    });
+
+    server.start(function() {
+      console.log(dateFormat(new Date(), format) + ' - Server started at: ' + server.info.uri);
+    });
+
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,19 @@
-var Webpack = require('webpack');
-var path = require('path');
-var pjson = require('./package.json');
-
+// Webpack config file
 module.exports = {
-    devtool: 'eval-source-map',
-    entry: [
-        'webpack-dev-server/client?http://localhost:8080',
-        'webpack/hot/dev-server',
-        './src/js/app.jsx'
-    ],
-
-    output: {
-        path: path.join(__dirname, 'js'),
-        filename: pjson.name + '.min.js',
-        publicPath: 'http://localhost:8080/js/'
-    },
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/
-        }, {
-            test: /\.jsx$/,
-            loaders: ['react-hot', 'babel'],
-            exclude: /node_modules/
-        }]
-    },
-    plugins: [
-        new Webpack.HotModuleReplacementPlugin()
+  entry: './src/js/app.jsx',
+  output: {
+    path: __dirname + '/static/js',
+    filename: 'bundle.js',
+    query: {
+          presets: ['es2015']
+        }
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx$/, /\.js$/],
+        loader: 'babel-loader'
+      }
     ]
+  },
 };
